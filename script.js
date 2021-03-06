4319511ace4f20a5beea0f145a1b8b033cdff0ce
Mhg1996 <77242548+Mhg1996@users.noreply.github.com>
2021-03-06 14:59:35 -0500

    started js

---
+++ script.js
@@ -0,0 +1,17 @@
+let apiKey = "3ac0d8db34de82819d13a9167239acc1";
+let searchBtn = $(".searchBtn");
+let searchInput = $(".searchInput");
+
+// Left column locations
+let cityNameEl = $(".cityName");
+let currentDateEl = $(".currentDate");
+let weatherIconEl = $(".weatherIcon");
+let searchHistoryEl = $(".historyItems");
+
+// Right column locations
+let tempEl = $(".temp");
+let humidityEl = $(".humidity");
+let windSpeedEl = $(".windSpeed");
+let uvIndexEl = $(".uvIndex");
+let cardRow = $(".card-row");
+
